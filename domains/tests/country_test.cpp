dbe0feb4d5cce40e85a8cf2fb25342fec23fe031
Fernando Jorge <fernando.jsts@gmail.com>
2022-07-01 20:37:40 -0300

    :white_check_mark: Add country test files
    Testes de criação, deleção, validação e invalidação

---
+++ domains/tests/country_test.cpp
@@ -0,0 +1,65 @@
+#include <string>
+#include <iostream>
+#include "country_test.hpp"
+using namespace std;
+
+int CountryTest::run()
+{
+    create();
+
+    test_validation("Turquia");
+    test_invalidation("São Paulo");
+
+    destroy();
+
+    return result;
+}
+
+void CountryTest::create()
+{
+    country = new Country("Brasil");
+    result = success;
+}
+
+void CountryTest::destroy()
+{
+    delete country;
+}
+
+void CountryTest::test_validation(string country)
+{
+    cout << "Espera-se que aceite a entrada" << endl;
+    try
+    {
+        cout << "País testado: " << country << endl
+             << endl;
+        this->country->setName(country);
+        cout << "País aceito!" << endl;
+    }
+    catch (invalid_argument &message)
+    {
+        cout << "País rejeitado!" << endl;
+        cout << "Mensagem de erro: " << message.what() << endl;
+        result = failure;
+    }
+    cout << "\n================================\n";
+}
+
+void CountryTest::test_invalidation(string country)
+{
+    cout << "Espera-se que rejeite a entrada" << endl;
+    try
+    {
+        cout << "País testado: " << country << endl
+             << endl;
+        this->country->setName(country);
+        cout << "País aceito!" << endl;
+        result = failure;
+    }
+    catch (invalid_argument &message)
+    {
+        cout << "País rejeitado!" << endl;
+        cout << "Erro: " << message.what() << endl;
+    }
+    cout << "\n================================\n";
+}
