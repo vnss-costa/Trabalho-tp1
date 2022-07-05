149459a5b997706980c3cc2a46d561bad327198f
Vanessa Costa <vanessapaixao@cjr.org.br>
2022-07-05 10:39:44 -0300

    feat: cria biblioteca da classe dominio

---
+++ domains/city.hpp
@@ -0,0 +1,63 @@
+#ifndef CITY
+#define CITY
+#include <string>
+#include <iostream>
+
+
+/**
+ * @brief Classe do domínio Idioma
+ * 
+ * @author Vanessa Paixão - 200028286
+ */
+
+ class City {
+     private:
+        inline static const cities[20] = 
+        {
+            "Antalya",
+            "Bangkok",
+            "Delhi",
+            "Dubai",
+            "Hong Kong",
+            "Londres",
+            "Macau",
+            "Mumbai",
+            "Paris",
+            "Rio de Janeiro",
+            "São Paulo",
+            "Seul",
+            "Istambul",
+            "Kuala Lumpur",
+            "Nova Iorque",
+            "Osaka",
+            "Phuket",
+            "Shenzhen",
+            "Toquio"
+        }
+        string city;
+        void validate(string city);
+     public:
+
+     City();
+
+     City(sting city);
+
+     string getCity();
+
+     void setName(string city); 
+ };
+
+ inline string City::getCity()
+ {
+     return city;
+ }
+
+ inline void City::setName(string city)
+ {
+     isValid(city);
+     this->city = city;
+ }
+
+ inline City::city() {};
+
+ #endif
