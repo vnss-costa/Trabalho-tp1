46dd7012e395bb75dfe26be39746ed595e14a102
PedroHVilarins <56796257+PedroHVilarins@users.noreply.github.com>
2022-07-05 00:07:36 -0300

    Nota

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "nota.h"
+
+using namespace std;
+
+int main()
+{
+    Nota a(1);
+    cout << a.getNota() << endl;
+    return 0;
+}
---
+++ nota.cpp
@@ -0,0 +1,17 @@
+#include "nota.h"
+
+//Valida a nota entre 0 e 10
+void Nota::validar(int nota) const{
+    if (nota < MIN || nota > MAX)
+        throw invalid_argument("Argumento invalido.");
+}
+
+Nota::Nota(int nota){
+    validar(nota);
+    this->nota = nota;
+}
+
+void Nota::setNota(int nota){
+    validar(nota);
+    this->nota = nota;
+}
