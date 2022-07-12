e31d6d8d48da5f45f931e5c79b4150b4db61108d
Vanessa Costa <vanessapaixao@cjr.org.br>
2022-07-11 23:30:12 -0300

    feat: cria biblioteca e função main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "main.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    CityTest city = CityTest();
+    if (country.run() == Main::failure) {
+        cout<< "Erro no teste de pais";
+        return Main::failure
+    }
+
+    cout<<"Testes finalizados";
+
+    return Main::success;
+}
---
+++ main.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string.h>
+
+#include "./domains/tests/country.hpp"
+
+class Main
+{
+    public: 
+        static const int success = 0;
+        static const int failure = 1;
+};
