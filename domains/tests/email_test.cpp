7001eedabaa4512d241a0bbc31dd1287b662465f
Fernando Jorge <fernando.jsts@gmail.com>
2022-07-18 10:45:13 -0300

    :memo: add texts to email test

--- domains/tests/email_test.cpp
+++ domains/tests/email_test.cpp
@@ -1,6 +1,8 @@
+#include <string>
+#include <iostream>
 #include "email_test.hpp"
+using namespace std;
 
-// Muda o valor "email(valor)" para testar email
 void EmailTest::setUp()
 {
     email = new Email("pedrovilarins077@gmail.com");
@@ -14,32 +16,46 @@ void EmailTest::tearDown()
 
 void EmailTest::testarCenarioSucesso()
 {
+    cout << "Espera-se que aceite a entrada" << endl;
     try
     {
+        cout << "Email testado: " << VALOR_VALIDO << endl
+             << endl;
         email->setEmail(VALOR_VALIDO);
         if (email->getEmail() != VALOR_VALIDO)
             estado = FALHA;
+        else
+            cout << "Email aceito!" << endl;
     }
-    catch (invalid_argument &excecao)
+    catch (invalid_argument &message)
     {
+        cout << "Email rejeitado!" << endl;
+        cout << "Mensagem de erro: " << message.what() << endl;
         estado = FALHA;
     }
+    cout << "\n================================\n";
 }
 
 void EmailTest::testarCenarioFalha()
 {
+    cout << "Espera-se que rejeite a entrada" << endl;
     try
     {
+        cout << "Email testado: " << VALOR_INVALIDO << endl
+             << endl;
         email->setEmail(VALOR_INVALIDO);
+        cout << "Email aceito! (Inesperado)" << endl;
         estado = FALHA;
     }
-    catch (invalid_argument &excecao)
+    catch (invalid_argument &message)
     {
+        cout << "Email rejeitado!" << endl;
+        cout << "Erro: " << message.what() << endl;
         if (email->getEmail() == VALOR_INVALIDO)
             estado = FALHA;
     }
+    cout << "\n================================\n";
 }
-
 int EmailTest::run()
 {
     setUp();
