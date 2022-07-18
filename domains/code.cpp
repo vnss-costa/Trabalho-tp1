4d70581805326382b65b1f22c2dbf68f41c3bcd1
Fernando Jorge <fernando.jsts@gmail.com>
2022-07-18 10:19:52 -0300

    :memo: Add text to idiom tests

--- domains/code.cpp
+++ domains/code.cpp
@@ -1,23 +1,20 @@
 #include "code.hpp"
 #include <string>
 
-void Codigo::validar(string codigo) const{
+void Codigo::validar(string codigo) const
+{
     if (codigo.length() != TAMANHO_CODIGO)
         throw invalid_argument("Tamanho Incorreto");
 
+    for (int i; i < codigo.length(); i++)
+    {
 
-    for(int i;i < codigo.length();i++){
-        //int temp = codigo[i] - '0'; //transforma o char 'num' para int num
-
-        if (isdigit(codigo[i])!= true)
+        if (isdigit(codigo[i]) != true)
             throw invalid_argument("Por favor, entre com um c�digo num�rico");
-
     }
-    //temporario
     int sum_digito, digito_verificador;
-    sum_digito = (codigo[0] - '0') + (codigo[1] - '0') + (codigo[2] - '0') + (codigo[3] - '0') + (codigo[4] - '0')+
+    sum_digito = (codigo[0] - '0') + (codigo[1] - '0') + (codigo[2] - '0') + (codigo[3] - '0') + (codigo[4] - '0') +
                  (codigo[5] - '0') + (codigo[6] - '0') + (codigo[7] - '0') + (codigo[8] - '0') + (codigo[9] - '0');
-
 
     digito_verificador = (codigo[POSICAO_DIGITO_VERIFICADOR] - '0');
 
@@ -25,14 +22,14 @@ void Codigo::validar(string codigo) const{
         throw invalid_argument("Digito verificador inv�lido");
 }
 
-Codigo::Codigo(string codigo){
+Codigo::Codigo(string codigo)
+{
     validar(codigo);
     this->codigo = codigo;
 }
 
-void Codigo::setCodigo(string codigo){
+void Codigo::setCodigo(string codigo)
+{
     validar(codigo);
     this->codigo = codigo;
 }
-
-
--- domains/tests/idiom_test.cpp
+++ domains/tests/idiom_test.cpp
@@ -1,4 +1,7 @@
+#include <string>
+#include <iostream>
 #include "idiom_test.hpp"
+using namespace std;
 
 void IdiomTest::setUp()
 {
@@ -13,32 +16,46 @@ void IdiomTest::tearDown()
 
 void IdiomTest::testarCenarioSucesso()
 {
+    cout << "Espera-se que aceite a entrada" << endl;
     try
     {
+        cout << "Idioma testado: " << VALOR_VALIDO << endl
+             << endl;
         idioma->setIdioma(VALOR_VALIDO);
         if (idioma->getIdioma() != VALOR_VALIDO)
             estado = FALHA;
+        else
+            cout << "Idioma aceito!" << endl;
     }
-    catch (invalid_argument &excecao)
+    catch (invalid_argument &message)
     {
+        cout << "Idioma rejeitado!" << endl;
+        cout << "Mensagem de erro: " << message.what() << endl;
         estado = FALHA;
     }
+    cout << "\n================================\n";
 }
 
 void IdiomTest::testarCenarioFalha()
 {
+    cout << "Espera-se que rejeite a entrada" << endl;
     try
     {
+        cout << "Idioma testado: " << VALOR_INVALIDO << endl
+             << endl;
         idioma->setIdioma(VALOR_INVALIDO);
+        cout << "Idioma aceito! (Inesperado)" << endl;
         estado = FALHA;
     }
-    catch (invalid_argument &excecao)
+    catch (invalid_argument &message)
     {
+        cout << "Idioma rejeitado!" << endl;
+        cout << "Erro: " << message.what() << endl;
         if (idioma->getIdioma() == VALOR_INVALIDO)
             estado = FALHA;
     }
+    cout << "\n================================\n";
 }
-
 int IdiomTest::run()
 {
     setUp();
