a039aefcb4eead04c697e817e467b5af01887426
Fernando Jorge <fernando.jsts@gmail.com>
2022-07-18 21:35:18 -0300

    :recycle: Standarlize name code

--- domains/tests/name_test.cpp
+++ domains/tests/name_test.cpp
@@ -11,48 +11,62 @@ using namespace std;
 
 void NameTest::create()
 {
-    name = new Name("Pedro Henrique Da Costa Vilari");
-    estado = SUCESSO;
+  name = new Name("Pedro Henrique");
+  estado = success;
 }
 
 void NameTest::destroy()
 {
-    delete name;
+  delete name;
 }
 
 void NameTest::test_validation()
 {
-    try
-    {
-        name->setName(VALOR_VALIDO);
-        if (name->getName() != VALOR_VALIDO)
-            estado = FALHA;
-    }
-    catch (invalid_argument &excecao)
-    {
-        estado = FALHA;
-    }
+  cout << "Espera-se que aceite a entrada" << endl;
+  try
+  {
+    cout << "Nome testado: " << VALOR_VALIDO << endl
+         << endl;
+    name->setName(VALOR_VALIDO);
+    if (name->getName() != VALOR_VALIDO)
+      estado = failure;
+    else
+      cout << "Nome aceito!" << endl;
+  }
+  catch (invalid_argument &message)
+  {
+    cout << "Nome rejeitado!" << endl;
+    cout << "Mensagem de erro: " << message.what() << endl;
+    estado = failure;
+  }
+  cout << "\n================================\n";
 }
 
 void NameTest::test_invalidation()
 {
-    try
-    {
-        name->setName(VALOR_INVALIDO);
-        estado = FALHA;
-    }
-    catch (invalid_argument &excecao)
-    {
-        if (name->getName() == VALOR_INVALIDO)
-            estado = FALHA;
-    }
+  cout << "Espera-se que rejeite a entrada" << endl;
+  try
+  {
+    cout << "Nome testado: " << VALOR_INVALIDO << endl
+         << endl;
+    name->setName(VALOR_INVALIDO);
+    cout << "Nome aceito!" << endl;
+    estado = failure;
+  }
+  catch (invalid_argument &message)
+  {
+    cout << "Nome rejeitado!" << endl;
+    cout << "Erro: " << message.what() << endl;
+    if (name->getName() == VALOR_INVALIDO)
+      estado = failure;
+  }
+  cout << "\n================================\n";
 }
-
 int NameTest::run()
 {
-    create();
-    test_validation();
-    test_invalidation();
-    destroy();
-    return estado;
+  create();
+  test_validation();
+  test_invalidation();
+  destroy();
+  return estado;
 }
--- domains/tests/name_test.hpp
+++ domains/tests/name_test.hpp
@@ -21,9 +21,10 @@ private:
     void destroy();
     void test_validation();
     void test_invalidation();
+
 public:
-    const static int SUCESSO =  0;
-    const static int FALHA   = -1;
+    const static int success = 0;
+    const static int failure = -1;
     int run();
 };
 
